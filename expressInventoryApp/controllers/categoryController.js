4c9a20084ec3dd8cd8bb0788d9be852c4d817ba6
McKenzie Jones <mckenzbeck12@gmail.com>
2024-01-29 18:19:59 -0700

    Add functionality to routers and hide MongoDB URI

--- expressInventoryApp/controllers/categoryController.js
+++ expressInventoryApp/controllers/categoryController.js
@@ -1,20 +1,38 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Categories
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category List");
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+  res.render("category_list", {
+    title: "All Categories",
+    category_list: allCategories,
+  });
 });
 
 // Display detail page for specific Category
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Category Detail: ${req.params.id}`);
+  // Get details of category and all associated items (in parallel)
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, "name description").exec(),
+  ]);
+  if (category === null) {
+    // No results
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("category_detail", {
+    category: category,
+    category_items: itemsInCategory,
+  });
 });
 
 // Display Category create form on GET
-exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category Create GET");
-});
+exports.category_create_get = asyncHandler(async (req, res, next) => {});
 
 // Handle Category create on POST
 exports.category_create_post = asyncHandler(async (req, res, next) => {
--- expressInventoryApp/controllers/itemController.js
+++ expressInventoryApp/controllers/itemController.js
@@ -1,18 +1,49 @@
+const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  const [numCategories, numItems] = await Promise.all([
+    Category.countDocuments({}).exec(),
+    Item.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Colorado Adventure Rentals Inc.",
+    categoryCount: numCategories,
+    itemCount: numItems,
+  });
 });
 
 // Display list of all Items.
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item list ");
+  const allItems = await Item.find({}, "name category")
+    .sort({ name: 1 })
+    .populate("category")
+    .exec();
+  res.render("item_list", {
+    title: "All Rental Options",
+    item_list: allItems,
+  });
 });
 
 // Display detail page for a specific Item.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Item detail: ${req.params.id}`);
+  console.log("is this showing?");
+  const rental = await Item.findById(req.params.id).populate("category").exec();
+
+  console.log("what is rental: ", rental);
+
+  if (rental === null) {
+    const err = new Error("Rental not found");
+    err.status = 404;
+    return next(Error);
+  }
+
+  res.render("item_detail", {
+    title: "Rental Details",
+    rental: rental,
+  });
 });
 
 // Display Item create form on GET
